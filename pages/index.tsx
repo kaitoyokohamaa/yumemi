63685217e58bb25d8bb0bec5a75fe51a1006c16c
yoko <s1A121700885@toyo.jp>
2021-02-25 11:46:07 +0900

    feat:fetch prefectures

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getPrefectureData } from "./api/prefecture";
+export interface PrefectureInfo {
+  prefCode: number;
+  prefName: string;
+}
 
 export default function index() {
-  return <div className="text-center bg-blue p-10">ホーム</div>;
+  const [prefectures, setPrefectures] = useState<PrefectureInfo[]>([]);
+  useEffect(() => {
+    const fetch = async () => {
+      const res = getPrefectureData.FetchPrefecture();
+      await res.then((data) => setPrefectures(data));
+    };
+    fetch();
+  }, []);
+  console.log(prefectures);
+  return (
+    <div className="mt-10">
+      <h1 className="w-9/12 m-auto">
+        <span className="border-2 border-black">都道府県</span>
+      </h1>
+      <div className="flex  flex-wrap w-9/12 m-auto">
+        {prefectures?.map((item) => {
+          return (
+            <label key={item.prefCode} className="flex mr-14 mt-4">
+              <input type="checkbox" />
+              <div>{item.prefName}</div>
+            </label>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
